fbb7f171f56b8f1a167c0ccb112ab1231d202a42
Joseph Hunkeler <jhunkeler@gmail.com>
2020-02-24 00:14:44 -0500

    Initial commit of resolve.c (deps.c replacement)

--- include/resolve.h
+++ include/resolve.h
@@ -1,8 +1,12 @@
-//
-// Created by jhunk on 2/22/20.
-//
-
+/**
+ * @file resolve.h
+ */
 #ifndef SPM_RESOLVE_H
 #define SPM_RESOLVE_H
+
+#define SPM_REQUIREMENT_MAX 1024
+
+int resolve_has_dependency(const char *archive);
+ManifestPackage **resolve_dependencies(ManifestList *manifests, const char *spec);
 
 #endif //SPM_RESOLVE_H
---
+++ src/resolve.c
@@ -0,0 +1,47 @@
+/**
+ * Dependency resolution functions
+ * @file resolve.c
+ */
+#include "spm.h"
+
+static ManifestPackage *requirements[SPM_REQUIREMENT_MAX] = {0, };
+
+/**
+ * Scan global `requirements` array for `archive`
+ * @param archive
+ * @return 0 = not found, 1 = found
+ */
+int resolve_has_dependency(const char *archive) {
+    for (size_t i = 0; requirements[i] != NULL && i < SPM_REQUIREMENT_MAX; i++) {
+        if (strcmp(requirements[i]->archive, archive) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/**
+ * Recursively scan a package for its dependencies
+ * @param manifests `ManifestList` struct
+ * @param spec Package name (accepts version specifiers)
+ * @return success = array of `ManifestPackage`, not found = NULL
+ */
+ManifestPackage **resolve_dependencies(ManifestList *manifests, const char *spec) {
+    ManifestPackage *package = manifestlist_search(manifests, spec);
+    if (package == NULL) {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < package->requirements_records && i < SPM_REQUIREMENT_MAX; i++) {
+        ManifestPackage *requirement = manifestlist_search(manifests, package->requirements[i]);
+        if (requirement == NULL) {
+            break;
+        }
+        if (resolve_has_dependency(requirement->archive)) {
+            continue;
+        }
+        resolve_dependencies(manifests, requirement->archive);
+        requirements[i] = requirement;
+    }
+    return requirements;
+}
