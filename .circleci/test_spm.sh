dad2485e1ff56073ae149d3caa5d702413318204
Joseph Hunkeler <jhunkeler@gmail.com>
2020-03-26 11:23:34 -0400

    More smoke tests

--- .circleci/test_spm.sh
+++ .circleci/test_spm.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-set -e
+#!/bin/bash -e
 
+export PREFIX=/tmp/root
 export SHELL=/bin/bash
 cd build
 
@@ -11,3 +11,26 @@ ctest3 -V
 spm --list
 
 spm --search zlib
+
+spm --verbose --yes --root $PREFIX --install python
+
+set +x
+echo ACTIVATING ROOT: $PREFIX
+spm --cmd mkruntime $PREFIX > $PREFIX/bin/activate || exit 1
+source $PREFIX/bin/activate || exit 1
+echo OK!
+set -x
+
+which python3
+
+python3 -V
+
+python3 -c 'from sysconfig import get_config_vars; from pprint import pprint; pprint(get_config_vars())'
+
+python3 -m ensurepip
+
+pip3 --version
+
+pip3 install --upgrade pip setuptools
+
+pip3 --version
