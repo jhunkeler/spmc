c604f63f4e20382ca802d103a60c99ff8aeadd35
Joseph Hunkeler <jhunkeler@gmail.com>
2020-04-20 01:47:37 -0400

    Omit installation test on MacOS

--- .circleci/test_spm.sh
+++ .circleci/test_spm.sh
@@ -13,25 +13,27 @@ spm --list
 
 spm --search zlib
 
-spm --verbose --yes --root $PREFIX --install python
+if [[ $(uname -s) == Linux ]]; then
+    spm --verbose --yes --root $PREFIX --install python
 
-set +x
-echo ACTIVATING ROOT: $PREFIX
-spm --cmd mkruntime $PREFIX > $PREFIX/bin/activate || exit 1
-source $PREFIX/bin/activate || exit 1
-echo OK!
-set -x
+    set +x
+    echo ACTIVATING ROOT: $PREFIX
+    spm --cmd mkruntime $PREFIX > $PREFIX/bin/activate || exit 1
+    source $PREFIX/bin/activate || exit 1
+    echo OK!
+    set -x
 
-which python3
+    which python3
 
-python3 -V
+    python3 -V
 
-python3 -c 'from sysconfig import get_config_vars; from pprint import pprint; pprint(get_config_vars())'
+    python3 -c 'from sysconfig import get_config_vars; from pprint import pprint; pprint(get_config_vars())'
 
-python3 -m ensurepip
+    python3 -m ensurepip
 
-pip3 --version
+    pip3 --version
 
-pip3 install --upgrade pip setuptools
+    pip3 install --upgrade pip setuptools
 
-pip3 --version
+    pip3 --version
+fi
