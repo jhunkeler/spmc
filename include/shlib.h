1f947b4786c310c13147400d737cd48bd2f05da4
Joseph Hunkeler <jhunkeler@gmail.com>
2020-04-24 12:27:52 -0400

    Add macos specific code

--- include/shlib.h
+++ include/shlib.h
@@ -4,13 +4,13 @@
 #ifndef SPM_SHLIB_H
 #define SPM_SHLIB_H
 
-#if defined(_MSC_VER)
+#if OS_WINDOWS && defined(_MSC_VER)
 #define SPM_SHLIB_EXEC "dumpbin"
 #define SPM_SHLIB_EXEC_ARGS "/dependents"
 #define SPM_SHLIB_EXTENSION ".dll"
-#elif defined(__APPLE__) && defined(__MACH__)
-#define SPM_SHLIB_EXEC "/usr/bin/otool"
-#define SPM_SHLIB_EXEC_ARGS "-l"
+#elif OS_APPLE
+#define SPM_SHLIB_EXEC "/usr/bin/objdump"
+#define SPM_SHLIB_EXEC_ARGS "-macho -p"
 #define SPM_SHLIB_EXTENSION ".dylib"
 #else  // linux (hopefully)
 #define SPM_SHLIB_EXEC "/usr/bin/objdump"
